6c1b8d1feb7bdf2fe4c421cb40367b06b916821d
Yoel <alfayoel0@gmail.com>
2026-06-03 13:09:25 +0200

    Add form for autoregistro of new tarjeta

---
+++ Página web/simulador.php
@@ -0,0 +1,6 @@
+<form action="incidencias.php" method="POST">
+    <input type="hidden" name="tarjeta_id" value="8888899999">
+    <button type="submit" style="padding: 20px; background: green; color: white;">
+        AUTOREGISTRO (NUEVA TARJETA)
+    </button>
+</form>
